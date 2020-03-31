de48936f4b14000dde8e68e7e68f1d8153ca8863
Shelley Dogra <sdogra@Shelleys-MacBook-Pro.local>
2020-03-31 08:49:47 -0500

    logo white, sitemap, meta desc done

--- source/js/script.js
+++ source/js/script.js
@@ -3,15 +3,6 @@
     'use strict';
   
     document.addEventListener('DOMContentLoaded', function() {
-      //company info
-      // var phoneIn = document.getElementById('phone');
-      // var emailIn = document.getElementById('email');
-      // var addressIn = document.getElementById('address');
-  
-      // addressIn.innerHTML = "1410 Bison Ln<br>Hoffman Estates, IL 60192";
-      // document.getElementById('phone').innerHTML = "(630) 788-3503";
-      // emailIn.innerHTML = "info@dlinelogistics.net";
-
       var cy = document.getElementById('current-year');
       var date = new Date();
       var year = date.getFullYear();
